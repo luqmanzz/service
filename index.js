f64141a1025245364ad48bd26b7084aab4397da2
luqmanzz <62497705+luqmanzz@users.noreply.github.com>
2020-07-12 18:02:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import router from './routerFile';
+import CreateAllTable from './src/db/createAllTable.js';
 
 const express = require('express')
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
+// use req.query to get param from url and req.body to get body params
+
+CreateAllTable();   // so that all tables are created and ready
 
 router(app);
 
