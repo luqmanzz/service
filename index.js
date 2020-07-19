7f2584434ab655f63a70552d80cf4595f5ff7951
luqmanzz <62497705+luqmanzz@users.noreply.github.com>
2020-07-19 18:43:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import router from './routerFile';
 import CreateAllTable from './src/db/createAllTable.js';
 
-const express = require('express')
+const express = require('express');
+const rateLimit = require("express-rate-limit");
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 // use req.query to get param from url and req.body to get body params
+const apiLimiter = rateLimit({
+    windowMs: 60 * 60 * 1000, // 15 minutes
+    max: 10,
+    message: "Too many attempts called from this IP, please try again after one hour"
+});
+app.use('/register-user', apiLimiter);
 
 CreateAllTable();   // so that all tables are created and ready
 
