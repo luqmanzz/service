5a435ec6a50c02d82d7c7c0ad622c49a053f1184
luqmanzz <62497705+luqmanzz@users.noreply.github.com>
2020-07-19 18:43:39 +0530

    Update routerFile.js

--- routerFile.js
+++ routerFile.js
@@ -1,6 +1,17 @@
 // all the router code will be placed here
 import InsertData from './src/db/CRUD.js';
 import validate from './src/login/validate.js';
+import sendOTP from './src/login/sendOTP.js';
+
+function unknownResponse(res) {
+    const errorMessage = 'Unknown error occured';
+    const result = {
+        status: 'failure',
+        message: errorMessage,
+        otp_status: 'unsuccessful'
+    }
+    res.send(result);
+}
 
 export default function router(app) {
 
@@ -10,30 +21,39 @@ export default function router(app) {
 
     app.post('/register-user', (req, res) => {
         let checkParam = validate('register-user', req);
-        if (!Object.keys(checkParam)) {
-            const result = {
-                status: 'failure',
-                message: checkParam[Object.keys(checkParam)],
-                otp_status: 'unsuccessful'
+        checkParam.then((checkParam) => {
+            if (Object.keys(checkParam).includes('false')) {
+                const result = {
+                    status: 'failure',
+                    message: checkParam[Object.keys(checkParam)],
+                    otp_status: 'unsuccessful'
+                }
+                res.send(result);
+            } else {
+                const number = `${req.body.mobile_number}`;
+                sendOTP(number).then((resp) => {    // works only for indian number
+                    req.body.OTP = resp;
+                    InsertData("register-user", req).then(response => {
+                        const result = {
+                            status: 'success',
+                            message: 'an OTP has been sent to your device',
+                            otp_status: 'successful'
+                        }
+                        res.send(result);
+                    }).catch(() => {
+                        unknownResponse(res);
+                    });
+                }).catch(() => {
+                    const result = {
+                        status: 'failure',
+                        message: 'OTP failed to send',
+                        otp_status: 'unsuccessful'
+                    }
+                    res.send(result);
+                });
             }
-            res.send(result);
-        }
-
-        InsertData("register-user", req).then(resp => {
-            const result = {
-                status: 'success',
-                message: 'an OTP has been sent to your device',
-                otp_status: 'successful'
-            }
-            res.send(result);
-        }).catch((err) => {
-            const errorMessage = err.toString().includes('duplicate key value violates unique constraint') ? 'device already registered' : 'unknown error occured';
-            const result = {
-                status: 'failure',
-                message: errorMessage,
-                otp_status: 'unsuccessful'
-            }
-            res.send(result);
+        }).catch(() => {
+            unknownResponse(res);
         });
     });
 }
