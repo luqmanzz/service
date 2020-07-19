ac77a3b0e1a11f833b29372e88fbb1265cd3003f
luqmanzz <62497705+luqmanzz@users.noreply.github.com>
2020-07-19 18:44:15 +0530

    Update validate.js

--- src/login/validate.js
+++ src/login/validate.js
@@ -1,13 +1,34 @@
-function validateUser(request) {
+import executeQuery from '../db/executeQuery.js';
+
+async function validateUser(request) {
     var body = request.body;
     let mobile = body.mobile_number;
     let countryCode = body.country_code;
-    let password = request.password;
-    return !validateCountryCode(countryCode) ? { false: 'country code is invalid' } :
-        (
-            !validateMobile(mobile) ? { false: 'mobile number is invalid' } :
-                ((!validatePassword(password)) ? { false: `password doesn't match password policy` } : { true: 'OK' })
-        );
+    let password = body.password;
+    const getVerified = constructVerifiedQuery(request);
+    return await executeQuery('users', getVerified).then((resp) => {
+        const result = (resp && resp.rows && resp.rows[0].verified === false) ? false : true;
+        if (!result) {
+            return !validateCountryCode(countryCode) ? { false: 'country code is invalid' } :
+                (
+                    !validateMobile(mobile) ? { false: 'mobile number is invalid' } :
+                        (!validatePassword(password) ? { false: `password doesn't match password policy` } : { true: 'OK' })
+                );
+        } else {
+            return ({
+                false: `device already registered`
+            });
+        }
+    }).catch(err => {
+        console.log(`error occured when getting verified data for `, mobile + err);
+        return ({
+            false: `unknown error occured`
+        });
+    });
+}
+
+function constructVerifiedQuery(request) {
+    return `select verified from users where mobile_number=${request.body.mobile_number}; `;
 }
 
 function validateCountryCode(code) {
@@ -22,6 +43,8 @@ function validateMobile(number) {
 
 function validatePassword(password) {
     let regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    console.log(regex.test(password) ? true : false);
+    console.log('pass', password)
     return regex.test(password) ? true : false;
 }
 
