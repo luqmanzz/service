6e6ee9e06cc5372a507dbf496ed9d79102df7aa2
luqmanzz <62497705+luqmanzz@users.noreply.github.com>
2020-07-23 19:13:29 +0530

    Update routerFile.js

--- routerFile.js
+++ routerFile.js
@@ -1,5 +1,5 @@
 // all the router code will be placed here
-import { InsertData, saveOTP } from './src/db/CRUD.js';
+import { InsertData, saveOTP, getOTP } from './src/db/CRUD.js';
 import validate from './src/login/validate.js';
 import sendOTP from './src/login/sendOTP.js';
 
@@ -20,7 +20,7 @@ export default function router(app) {
         res.send('Hello World!');
     });
 
-    app.get('/send-otp', (req, res) => {
+    app.post('/send-otp', (req, res) => {
         let checkParam = validate('send-otp', req);
         if (Object.keys(checkParam).includes('false')) {
             const result = {
@@ -51,8 +51,40 @@ export default function router(app) {
         }
     });
 
-    app.get('/verify-otp', (req, res) => {
-        res.send('Hello World!');
+    app.post('/verify-otp', (req, res) => {
+        let checkParam = validate('verify-otp', req);
+        if (Object.keys(checkParam).includes('false')) {
+            const result = {
+                status: 'failure',
+                message: checkParam[Object.keys(checkParam)]
+            }
+            res.send(result);
+        } else {
+            getOTP('verify-otp', req).then((resp) => {
+                const dbOTP = resp && resp.otp;
+                if (dbOTP === req.body.otp) {
+                    const result = {
+                        status: 'success',
+                        message: 'mobile number verified'
+                    }
+                    res.send(result);
+                } else if (!resp) {
+                    const result = {
+                        status: 'failure',
+                        message: 'number does not exist in records'
+                    }
+                    res.send(result);
+                } else {
+                    const result = {
+                        status: 'failure',
+                        message: 'incorrect otp'
+                    }
+                    res.send(result);
+                }
+            }).catch(() => {
+                unknownResponse(res, 'cant fetch OTP from db');
+            });
+        }
     });
 
     app.post('/register-user', (req, res) => {
